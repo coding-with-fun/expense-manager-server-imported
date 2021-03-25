a1f93685ecdd9d29aa56ea081c525be63df9eef2
Coderc <dev@coderc.in>
2021-03-26 00:33:16 +0530

    Changed namecasing

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,7 +15,7 @@ exports.authenticateToken = () => {
             algorithms: ['HS256'],
             userProperty: 'auth',
             getToken: function fromHeaderOrQuerystring(req) {
-                const token = req.headers.authToken;
+                const token = req.headers.authtoken;
                 console.log('req :: ', req, 'token :: ', token);
                 if (token && token.split(' ')[0] === 'Bearer') {
                     console.log(
