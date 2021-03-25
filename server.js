a7ed5e33bddb646e63ee41f60f7b989619282486
Coderc <dev@coderc.in>
2021-03-25 10:07:36 +0530

    Changed PORT

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const indexRoutes = require('./router/router');
 /**
  *  @description Defining variables.
  */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 /**
