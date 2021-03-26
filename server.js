6330d06dfd8ba93b603dbfddd78023d36e0c0d10
Coderc <dev@coderc.in>
2021-03-26 10:05:24 +0530

    Test cors 1

--- server.js
+++ server.js
@@ -30,7 +30,26 @@ const app = express();
  */
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: '*' }));
+// app.use(cors({ credentials: true }));
+
+app.use((req, res, next) => {
+    const allowedOrigins = [
+        'http://127.0.0.1:8020',
+        'http://localhost:8020',
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+        'https://expense-manager-dtw5tn0jb-coderc.vercel.app',
+    ];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+});
 
 /**
  *  @description Establishing Server Connection.
