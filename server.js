66439384677f55dc6abe1df780687bc25d2d652d
Coderc <dev@coderc.in>
2021-03-26 10:12:15 +0530

    Test cors 4

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use((req, res, next) => {
     ];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
+        console.log('CORS');
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
