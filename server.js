547771bb6cedd6e7a26c773489bfd7240bd32ceb
Coderc <dev@coderc.in>
2021-03-26 10:09:20 +0530

    Test cors 2

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const app = express();
  */
 app.use(bodyParser.json());
 app.use(cookieParser());
-// app.use(cors({ credentials: true }));
+app.use(cors());
 
 app.use((req, res, next) => {
     const allowedOrigins = [
@@ -45,8 +45,11 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // res.header(
+    //     'Access-Control-Allow-Methods',
+    //     'GET, OPTIONS, POST, PUT, DELETE'
+    // );
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
     return next();
 });
